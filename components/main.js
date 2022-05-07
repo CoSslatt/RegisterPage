5703655691ad6ca9e334771042ee416a3b908775
Narcikss <mdgamedeveloper@gmail.com>
2022-05-07 12:17:26 +0200

    commit

--- components/main.js
+++ components/main.js
@@ -1,5 +1,21 @@
 const loginForm = document.querySelector('.login__form');
+const input = document.querySelectorAll('.input');
+
+let account = {
+    name: 'Zbyszek',
+    password: '123'
+};
 
 loginForm.addEventListener('submit', e => {
     e.preventDefault();
+
+    loginFunc();
 })
+
+function loginFunc() {
+    if (input[0].value === 'Zbyszek' && input[1].value === '123') {
+        alert("Connected");
+    }
+    else
+        alert('Wrong name or password');
+};
