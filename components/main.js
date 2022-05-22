d0032aea1baee3211c2036c457ae38b1fb51c560
Narcikss <mdgamedeveloper@gmail.com>
2022-05-22 20:57:13 +0200

    add

--- components/main.js
+++ components/main.js
@@ -1,21 +1,25 @@
-const loginForm = document.querySelector('.login__form');
-const input = document.querySelectorAll('.input');
+const profileIcon = document.querySelector(".profile__logo");
+const profileToTrigger = document.querySelector(".profile__disable");
 
-let account = {
-    name: 'Zbyszek',
-    password: '123'
+const USER_ID_DIV = document.querySelector(".user__id");
+
+profileIcon.addEventListener("click", (e) => {
+	profileToTrigger.classList.toggle("profile__content__to__wrap");
+});
+
+const loadData = (callback) => {
+	const request = new XMLHttpRequest();
+
+	request.addEventListener("readystatechange", (e) => {
+		const data = JSON.parse(request.responseText);
+
+		callback(data);
+
+		request.open("GET", "components/users.json");
+		request.send();
+	});
 };
 
-loginForm.addEventListener('submit', e => {
-    e.preventDefault();
-
-    loginFunc();
-})
-
-function loginFunc() {
-    if (input[0].value === 'Zbyszek' && input[1].value === '123') {
-        alert("Connected");
-    }
-    else
-        alert('Wrong name or password');
-};
+loadData((data) => {
+	console.log(data);
+});
