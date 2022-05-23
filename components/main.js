e14a67b5333a3b0b638d9952097b12870a1eb0d0
Narcikss <mdgamedeveloper@gmail.com>
2022-05-23 21:44:42 +0200

    nothing

--- components/main.js
+++ components/main.js
@@ -1,25 +1,9 @@
 const profileIcon = document.querySelector(".profile__logo");
 const profileToTrigger = document.querySelector(".profile__disable");
+const formSubmit = document.querySelector(".submit");
 
 const USER_ID_DIV = document.querySelector(".user__id");
 
 profileIcon.addEventListener("click", (e) => {
 	profileToTrigger.classList.toggle("profile__content__to__wrap");
-});
-
-const loadData = (callback) => {
-	const request = new XMLHttpRequest();
-
-	request.addEventListener("readystatechange", (e) => {
-		const data = JSON.parse(request.responseText);
-
-		callback(data);
-
-		request.open("GET", "components/users.json");
-		request.send();
-	});
-};
-
-loadData((data) => {
-	console.log(data);
 });
